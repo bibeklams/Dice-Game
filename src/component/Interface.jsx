68d0c2f72dc0333a67287462c4d6121a803f15f9
bibek Tamang <bibeklama123asd@gmail.com>
2025-06-13 14:34:46 +0545

    add screenshot

--- src/component/Interface.jsx
+++ src/component/Interface.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import style from "./interface.module.css";
-import image from "../assets/dice.jpg";
+import images from "../assets/dice.jpg";
 
 import { useNavigate } from "react-router-dom";
 const Interface = () => {
@@ -11,7 +11,7 @@ const Interface = () => {
   return (
     <div className={style.contain}>
       <div className={style.image}>
-        <img src={image} alt="dice image" />
+        <img src={images} alt="dice image" />
       </div>
       <div className={style.game}>
         <h1>DICE GAME</h1>
